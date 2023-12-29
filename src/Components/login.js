abd8cb14cbda7471a0defc54d72587cdfd5de9fb
vinaya-sv <95991141+vinaya-sv@users.noreply.github.com>
2023-12-29 17:43:54 +0530

    Initial login page set up

---
+++ src/Components/login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/login.css';
+function Login(){
+    return(
+        <div className="login-wrapper">
+        Hai
+        </div>
+    );
+}
+export default Login;
